bf4c8e67dfb81b5d210fb6e8d890aad5978e3be8
fujita_m <fujita_m@works-hi.co.jp>
2023-01-12 10:03:24 +0900

    week1 コメン対応

--- src/main/java/readablecode/week1/Strings.java
+++ src/main/java/readablecode/week1/Strings.java
@@ -3,30 +3,13 @@ package readablecode.week1;
 public class Strings {
 	// TODO 1: 7.5,7.7 and 9.1
 	public static String capitalize(String str) {
-		// 変数を削除する(retという変数)
 		//関数の先頭に異常系となる条件を集めて即座にreturnする
-		if(str==null || str.length()==0) return str;
-		//ネストを浅くする
+		if(str==null || str.length()==0) {
+			return str;
+		}
 		if(str.length()==1) {
 			return str.toUpperCase();
-		}else {
-			return str.substring(0, 1).toUpperCase() + str.substring(1);
 		}
+		return str.substring(0, 1).toUpperCase() + str.substring(1);
 	}
 }
-	
-//既存のコード
-//	String ret = str;
-//	if (str == null) {
-//		ret = str;
-//	} else if (str.length() == 0) {
-//		ret = str;
-//	} else {
-//		if (str.length() == 1) {
-//			ret = str.toUpperCase();
-//		} else {
-//			ret = str.substring(0, 1).toUpperCase() + str.substring(1);
-//		}
-//	}
-//	return ret;
-//}
